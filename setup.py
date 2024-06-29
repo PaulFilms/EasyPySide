324c74757a458f718142c0959cd7f29359b4c643
Pablo Gonzalez <112630835+PaulFilms@users.noreply.github.com>
2024-06-29 10:17:02 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='lib_test',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Pablo Pila',
+      # author_email='gward@python.net',
+      # url='https://www.python.org/sigs/distutils-sig/',
+      packages=['modules'],
+     )
