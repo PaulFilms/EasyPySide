6030045bdb5289080e580c65cbae1c827b4ca624
Pablo Gonzalez <112630835+PaulFilms@users.noreply.github.com>
2024-07-06 20:04:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name='lib_test',
-      version='1.2',
+      version='1.3',
       description='Python Distribution Utilities',
       author='Pablo Pila',
       # author_email='gward@python.net',
       # url='https://www.python.org/sigs/distutils-sig/',
-      packages=find_packages(where='lib_test'),
+      packages=find_packages(include=['extra', '*.m4a', '*.py']),
      )
