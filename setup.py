33ca241d751e49a565c4670d67beccaa3eed2ee7
Pablo Gonzalez <112630835+PaulFilms@users.noreply.github.com>
2024-07-06 20:09:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='lib_test',
-      version='1.3',
+      version='1.4',
       description='Python Distribution Utilities',
       author='Pablo Pila',
       # author_email='gward@python.net',
