eb0b2d99832faaa425de9b04af4a8e2c1972cfd2
Pablo Gonzalez <112630835+PaulFilms@users.noreply.github.com>
2024-07-06 20:22:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='lib_test',
-      version='1.5',
+      version='1.6',
       description='Python Distribution Utilities',
       author='Pablo Pila',
       # author_email='gward@python.net',
       # url='https://www.python.org/sigs/distutils-sig/',
       packages=find_packages(),
       include_package_data=True, # muy importante para que se incluyan archivos sin extension .py
-      package_data={'lib_test': ['lib_test/*', 'extra/*']}, 
+      package_data={'lib_test': ['extra/*']}, 
 )
