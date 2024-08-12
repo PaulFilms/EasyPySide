8ecee1c2811e97e610f439fea1581fa2547a628f
PaulFilms <pablopila.spain@gmail.com>
2024-08-12 12:00:19 +0200

    forms_ui

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='lib_test',
-      version='1.6',
+      version='1.7',
       description='Python Distribution Utilities',
       author='Pablo Pila',
       # author_email='gward@python.net',
       # url='https://www.python.org/sigs/distutils-sig/',
       packages=find_packages(),
       include_package_data=True, # muy importante para que se incluyan archivos sin extension .py
-      package_data={'lib_test': ['forms/*']}, 
+      package_data={'lib_test': ['forms_ui/*']}, 
 )
