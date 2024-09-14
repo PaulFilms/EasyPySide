3d47dceb570f7a786f46b5c762bd80088db5e1cb
Pablo Gonzalez <112630835+PaulFilms@users.noreply.github.com>
2024-09-14 22:06:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-      name='lib_test',
+      name='easypyside',
       version='0.77',
       description='Python PySide6 Utilities',
       long_description = "README.md",
       author='Pablo GP',
       author_email='pablogonzalezpila@gmail.com',
-      url='https://github.com/PaulFilms/lib_test',
+      url='https://github.com/PaulFilms/easypyside',
       packages=find_packages(),
-      include_package_data=True, # muy importante para que se incluyan archivos sin extension .py
-      package_data={'lib_test': ['__forms/*']}, 
+      include_package_data=True,
+      package_data={'easypyside': ['__forms/*']}, 
 )
 
